8063ad23501578cba6244d74999f1cdc54f8d27e
Luis Gonzalez <l.adrian877@gmail.com>
2026-02-07 18:33:54 +0100

    Agregando coleccion de libros

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content';
+
+const booksCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    img: z.string(),
+    readtime: z.number(),
+    description: z.string(),
+    buy: z.object({
+      spain: z.string().url(),
+      usa: z.string().url(),
+    }),
+  }),
+});
+
+export const collections = {
+  books: booksCollection,
+};
